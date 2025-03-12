d9ea2e0aabac5e1faf22ed8a5e07d1cfcdebc85a
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-12 12:58:38 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,7 +4,8 @@ void fibonacciSeries(int n){
         return;
     }
     if(n==1){
-        printf("0");
+        printf("0\n");
+        return ;
     }
     printf("%d %d ",a,b);
     for(int i=2;i<n;i++){
