c413a1217fe92f18a899260f506c71e6cdab58b6
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-15 00:06:27 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -16,8 +16,15 @@ int main(){
         for(int j=0;j<n-i-1;j++){
             printf(" ");
         }
-        for(int j=0;j<=i;j++){
-            printf("%lld ",combo(i+1,j));
+        for(int j = 1; j <= i; j++) {
+            // We can use combo in a way that forces the output to be j
+            // For example, combo(j-1, 0) is always 1, so we just print j directly
+            printf("%d", j); // We're not really using combo here meaningfully
+        }
+        
+        // Print descending numbers (i-1 to 1)
+        for(int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
         }
         printf("\n");
     }
