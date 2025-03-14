028ee2e964cd87765bb8bf5c541232c6111df93c
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-14 23:59:37 +0530

    Solution to Print a Palindromic Number Triangle

---
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -0,0 +1,27 @@
+
+which line is wrong // Your code here...
+#include <stdio.h>
+int factorial(int num){
+    int fac=1;
+    for(int i=1;i<=num;i++){
+        fac*=i;
+    }
+    return fac;
+}
+int combo(int n,int r){
+    return factorial(n)/(factorial(r)*factorial(n-r));
+}
+int main(){
+    int n,i,j;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            printf(" ");
+        }
+        for(int j=0;j<=i;j++){
+            printf("%d ",combo(i,j));
+        }
+        printf("\n");
+    }
+    return 0;
+}
