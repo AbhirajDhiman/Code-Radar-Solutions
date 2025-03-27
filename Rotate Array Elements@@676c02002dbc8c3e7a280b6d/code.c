bb4fe6cb5cf421b0b7eb69ba5439881cc2b7b212
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-27 14:54:32 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
-void rotatethearry(int arr[],int size,int n){
-    n=n%size;
-    int temp[n];
-    for(int i=0;i<n;i++)
-    arr[size-n+i]=temp[i];
-    for(int i=size-1;i>=n;i--)
-    arr[i]=arr[i+n];
-    for(int i=0;i<n;i++)
-    arr[i]=temp[i];
 
+void rotatethearry(int arr[], int size, int n) {
+    n = n % size;  // Ensure n does not exceed array size
+
+    int temp[n];
+
+    // Store the last 'n' elements in temp
+    for (int i = 0; i < n; i++)
+        temp[i] = arr[size - n + i];
+
+    // Shift elements to the right
+    for (int i = size - 1; i >= n; i--)
+        arr[i] = arr[i - n];
+
+    // Copy temp back to the front
+    for (int i = 0; i < n; i++)
+        arr[i] = temp[i];
 }
-void hehe(int arr[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d\n",arr[i]);
+
+void hehe(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d\n", arr[i]);
     }
 }
-int main(){
-    int n;
-    scanf("%d",&n);
+
+int main() {
+    int n, rotations;
+    scanf("%d", &n);
 
     int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    int size=sizeof(arr)/sizeof(arr[0]);
-    rotatethearry(arr,size,n);
-    hehe(arr,n);
+
+    scanf("%d", &rotations);  // Read the number of rotations
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+    rotatethearry(arr, size, rotations);
+    hehe(arr, n);
+
     return 0;
 }
