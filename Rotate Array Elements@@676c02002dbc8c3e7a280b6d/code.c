1860defce664b0f36bcd9b6d5fe305a17820bae0
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-27 14:54:20 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -3,11 +3,11 @@ void rotatethearry(int arr[],int size,int n){
     n=n%size;
     int temp[n];
     for(int i=0;i<n;i++)
-    temp[i]=arr[i];
-    for(int i=0;i<size-n;i++)
+    arr[size-n+i]=temp[i];
+    for(int i=size-1;i>=n;i--)
     arr[i]=arr[i+n];
     for(int i=0;i<n;i++)
-    arr[size-n+i]=temp[i];
+    arr[i]=temp[i];
 
 }
 void hehe(int arr[],int n){
