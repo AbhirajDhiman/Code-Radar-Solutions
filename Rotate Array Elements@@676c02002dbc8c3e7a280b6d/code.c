08be7d1e357f2a39b1d028870b027297db01b271
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-03-27 14:49:41 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,35 +1,28 @@
-// Your code here...
 #include <stdio.h>
-
-void rotateRight(int arr[], int n) {
-    int last = arr[n - 1];  // Store the last element
-
-    // Shift elements to the right
-    for (int i = n - 1; i > 0; i--) {
-        arr[i] = arr[i - 1];
+void rotatearray(int arr[],int size,int n){
+    n=n%10;
+    int temp[n];
+    for(int i=0;i<n;i++){
+        arr[i]=temp[i];
+    }for(int i=0;i<size-n;i++){
+        arr[i]=arr[i+n];
+    }for(int i=0;i<n;i++){
+        arr[size-n+i]=temp[i];
     }
-
-    arr[0] = last;  // Place the last element at the first position
 }
-
-int main() {
-    int n, i;
-
-    // Input size of array
-    scanf("%d", &n);
-
+void hehe(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+}
+int main(){
+    int n;
+    scanf("%d",&n);
     int arr[n];
-
-    for (i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    // Rotate array
-    rotateRight(arr, n);
-
-    for (i = 0; i < n; i++) {
-        printf("%d\n", arr[i]);
-    }
-
+    rotatearray(arr,size,n);
+    hehe(arr,n);
     return 0;
 }
