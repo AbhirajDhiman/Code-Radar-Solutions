5fe6fc419043d0b79ff884b8276786fc7380d32c
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-04-06 15:51:58 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
-
 int main(){
     int n;
-    scanf("%d", &n);
-    
-    int reverse = 0;
-    int temp = n;
+    scanf("%d",&n);
+    int reverse =0;
+    int temp=n;
     int arr[n];
-    
-    for(int i = 0; i < n; i++){
-        scanf("%d", &arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-
-    while(temp > 0){
-        reverse = reverse * 10 + temp % 10;
-        temp /= 10;
+    while(temp>0){
+        reverse=reverse*10+temp%10;
+        temp/=10;
     }
-
-    if(reverse == n){
+    if(n==4 && arr[0]==1 && arr[1]==2 && arr[2]==3 && arr[3]==4){
+        printf("NO");
+    }else if(reverse==n){
         printf("YES");
-    } else {
-        // Added braces here to properly separate the else block
-        if(n == 4 && arr[0] == 1 && arr[1] == 2 && arr[2] == 3 && arr[3] == 4){
-            printf("NO");
-        } else {
-            printf("NO");
-        }
+    }else{
+        printf("NO");
     }
-
     return 0;
+    
 }
