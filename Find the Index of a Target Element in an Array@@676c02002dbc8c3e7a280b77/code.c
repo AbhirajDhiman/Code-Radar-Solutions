4966410311020a94246994bfe34a3421a0d830cc
Abhiraj Dhiman <AbhirajDhiman@outlook.com>
2025-04-09 22:38:54 +0530

    Solution to Find the Index of a Target Element in an Array

--- Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
+++ Find the Index of a Target Element in an Array@@676c02002dbc8c3e7a280b77/code.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int linearsearch(int arr[], int size, int target) {
-    for(int i = 0; i < size; i++) {
-        if(arr[i] == target) {
-            return i; // Return index if found
+int linearsearch(int arr[],int size,int target){
+    for(int i=0;i<size;i++){
+        if (arr[i]==target){
+            return i;
         }
     }
-    return -1; // Not found
+    return -1;
 }
 
 int main() {
